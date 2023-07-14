f3ad1dd9d9b5c1fbcb27ef964a874a364270c9e3
Vanchenko <vanchenkoev@pakline.com.ua>
2023-07-14 18:14:43 +0300

    first update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { ContactFind } from './ContactFind/ContactFind';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAllContacts } from 'redux/operations';
+import { selectIsLoading, selectError } from 'redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchAllContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <ContactFind />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactList />
     </div>
   );
 };
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import { ContactItem } from '../ContactItem/ContactItem';
+import { useSelector } from 'react-redux';
+import css from './ContactList.module.css';
+import { selectFilteredContacts } from 'redux/selectors';
+
+export const ContactList = () => {
+  const contacts = useSelector(selectFilteredContacts);
+  //const contacts = useSelector(getContacts);
+  console.log('Contacts List', contacts);
+  return (
+    <ul className={css.contacts_list}>
+      {contacts.map(contact => (
+        <ContactItem
+          key={contact.id}
+          id={contact.id}
+          name={contact.name}
+          number={contact.number}
+        />
+      ))}
+    </ul>
+  );
+};
